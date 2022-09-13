54f0f141247ef3e5b68ce9c368a204f4affc5d5d
=LocatelliBryann <=bryannlocatelli@gmail.com>
2022-09-13 14:30:03 -0300

    a

--- livraria/settings.py
+++ livraria/settings.py
@@ -115,8 +115,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REST_FRAMEWORK = {
