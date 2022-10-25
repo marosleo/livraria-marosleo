e2a80caa47cbb3e932fb260348a3ce56359c7c66
LocatelliBryann <bryannlocatelli@gmail.com>
2022-10-25 14:04:46 -0300

    Ajustado URLS

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,34 +1,31 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from core.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,)
+from media.router import router as media_router
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
-from media.router import router as media_router
 
 router = DefaultRouter()
-router.register(r"autores", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
-router.register(r"livros", LivroViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"livro", LivroViewSet)
 
 urlpatterns = [
-    # Simple JWT
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # Admin
-    path("admin/", admin.site.urls),
-    # Upload de imagens e documentos
     path("api/media/", include(media_router.urls)),
-    # OpenAPI 3
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
@@ -40,7 +37,6 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    # API REST
     path("api/", include(router.urls)),
 ]
 
